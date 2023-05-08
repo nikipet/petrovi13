05afc62abffa2f862eba7cf93673d6c0306a5d9c
nvpetrov93@gmail.com <nvpetrov93@gmail.com>
2023-05-08 17:35:39 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+ * Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+ */
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+function getCurrentYear() {
+  const currentDate = new Date();
+  return currentDate.getFullYear();
+}
+
+// Set the content of the element with the ID "current-year" to the current year
+document.getElementById("copyright").innerHTML = "Copyright&copy;   Петрови-13&reg; " + getCurrentYear();
